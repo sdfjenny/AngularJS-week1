75a1518d6947f15fd61523baa1d30a4e8cc8c9fd
Danfeng Shao <danfeng.shao@udg.de>
2019-01-15 10:51:56 +0100

    update

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
 (function(){
 'use strict';
 
-angular.module('DIApp', [])
+angular.module('LunchCheck', [])
 
-.controller('DIController', DIController);
+.controller('LunchCheckController', LunchCheckController);
 
-function DIController ($scope, $filter){
-	$scope.name = "Yaakov";
-	
-	$scope.upper = function() {
-		var upCase = $filter('uppercase');
-		$scope.name = upCase($scope.name);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+	$scope.name = 'list comma separated dishes you usually have for lunchs';
+	$scope.message = '';
+	$scope.check = function (){
+	 var nametocheck = $scope.name.trim();
+	 var messagetoshow = $scope.message;
+	 if (nametocheck) {
+	 var word = nametocheck.split(',');
+	 
+	 var i;
+	 var n=0;
+	 for (i = 0; i < word.length; i++){
+		 if (word[i].trim()){
+			 n=n+1;
+		 } else { n=n+0;}
+		 
+	 }
+	 
+	 console.log(n);
+	 
+	 
+	 if  (n == 0){
+		messagetoshow = 'Please enter data first';
+	} else if  (n == 3 || n < 3 ) {
+		messagetoshow = 'Enjoy!';
+	 } else if (n > 3){
+		 messagetoshow = 'Too much!';
+	 }
+	 }
+	 else {
+		messagetoshow = 'Please enter data first';
+	}
+	 console.log(messagetoshow);
+	    
+	$scope.message =  messagetoshow;
+		
 	};
 	
-}
 	
-
+	
+	
+}
 
 
 })();
